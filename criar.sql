b5e7f7c3b028ca5d5a9129c24e38b803b3fb6db6
Vinicius <vinimcorrea@hotmail.com>
2022-01-13 01:04:49 +0000

    added second delivery

--- criar.sql
+++ criar.sql
@@ -1,110 +1,112 @@
 DROP TABLE IF EXISTS Pessoa;
 CREATE TABLE Pessoa(
-	idPessoa INTEGER PRIMARY KEY, 	
-	nome VARCHAR(30) NOT NULL, 
-	altura INTEGER, 
-	genero CHAR(1) CHECK (genero in ('M', 'F')), 
-	peso REAL, 
-	nacionalidade VARCHAR(20) NOT NULL
+	IdPessoa INTEGER PRIMARY KEY, 	
+	Nome VARCHAR(30), 
+	Altura INTEGER, 
+	Genero CHAR(1) CONSTRAINT generoPessoa CHECK(Genero in ('M', 'F' )),
+	Peso REAL, 
+	Idade INT,
+	Nacionalidade VARCHAR(20) 
 );
 
 DROP TABLE IF EXISTS ComitivaOlimpica;
 CREATE TABLE  ComitivaOlimpica(
-	idComitiva INTEGER PRIMARY KEY, 
-	pais VARCHAR(30)  NOT NULL, 
-	desporto VARCHAR(15)  CHECK (desporto in ('Triatlo')) NOT NULL	
+	IdComitiva INTEGER PRIMARY KEY, 
+	Pais VARCHAR(30), 
+	Desporto VARCHAR(15)  CONSTRAINT desportoComitiva CHECK (Desporto in ('Triatlo'))
 );
 
 DROP TABLE IF EXISTS Tecnico;
 CREATE TABLE Tecnico(
-	idComitiva REFERENCES ComitivaOlimpica, 
-    idPessoa REFERENCES Pessoa,
-	PRIMARY KEY(idComitiva, idPessoa)
+	IdComitiva REFERENCES ComitivaOlimpica ON DELETE SET NULL ON UPDATE CASCADE, 
+    IdTecnico REFERENCES Pessoa ON DELETE SET NULL ON UPDATE CASCADE,
+	PRIMARY KEY(IdComitiva, IdTecnico)
 );
 
 DROP TABLE IF EXISTS Arbitro;
-CREATE TABLE Arbitro( 
-	idPessoa REFERENCES Pessoa, 
-	tipo VARCHAR(15) NOT NULL CONSTRAINT tipoArbitro CHECK(tipo in ('Geral', 'Natação', 'Transição', 'Ciclismo', 'Corrida', 'Cronometragem', 'Chegada'))
+CREATE TABLE Arbitro(
+	IdArbitro REFERENCES Pessoa ON DELETE SET NULL ON UPDATE CASCADE,
+	Tipo VARCHAR(15) NOT NULL CONSTRAINT tipoArbitro CHECK(Tipo in ('Geral', 'Natação', 'Transição', 'Ciclismo', 'Corrida', 'Cronometragem', 'Chegada')),
+	PRIMARY KEY(IdArbitro)
 );
 
 DROP TABLE IF EXISTS Atleta;
 CREATE TABLE Atleta(
-	idAtleta INTEGER PRIMARY KEY,
-	idComitiva REFERENCES ComitivaOlimpica, 
-	idPessoa REFERENCES Pessoa,
-	idCompeticao REFERENCES Competicao
+	IdComitiva REFERENCES ComitivaOlimpica ON DELETE SET NULL ON UPDATE CASCADE, 
+	IdAtleta REFERENCES Pessoa ON DELETE SET NULL ON UPDATE CASCADE,
+	IdCompeticao REFERENCES Competicao ON DELETE SET NULL ON UPDATE CASCADE,
+	PRIMARY KEY(IdAtleta)
 );
 
 DROP TABLE IF EXISTS Prova;
 CREATE TABLE Prova(
-	idProva INTEGER PRIMARY KEY, 
-	data TEXT, 
-	tipo VARCHAR(15) NOT NULL CONSTRAINT tipoProva CHECK (tipo in ('Ciclismo', 'Natação', 'Corrida'))
+	IdProva INTEGER PRIMARY KEY, 
+	IdCompeticao REFERENCES Competicao ON DELETE SET NULL ON UPDATE CASCADE,
+	Tipo VARCHAR(15) NOT NULL CONSTRAINT tipoProva CHECK (Tipo in ('Ciclismo', 'Natação', 'Corrida')) 
 	);
 
 DROP TABLE IF EXISTS Ocorrencia;
 CREATE TABLE Ocorrencia(
-	idOcorrencia INTEGER PRIMARY KEY, 
-	tempo TEXT, 	
-	descricao TEXT DEFAULT 'Sem Descrição'
+	IdOcorrencia INTEGER PRIMARY KEY, 
+	IdAtleta REFERENCES Atleta ON DELETE SET NULL ON UPDATE CASCADE,
+	IdCompeticao REFERENCES Competicao ON DELETE SET NULL ON UPDATE CASCADE,
+	Tempo TEXT, 	
+	Descricao TEXT DEFAULT 'Sem Descrição'
 );
 
 DROP TABLE IF EXISTS Cartao;
 CREATE TABLE Cartao(	
-	IDCartao REFERENCES Ocorrencia, 
-	IDAtleta REFERENCES Atleta, 
-	cor VARCHAR(10) CHECK(cor in ('Amarelo', 'Vermelho')), 
-	PRIMARY KEY(IDCartao, IDAtleta)
+	IdCartao REFERENCES Ocorrencia ON DELETE SET NULL ON UPDATE CASCADE, 
+	Cor VARCHAR(10) CHECK(Cor in ('Amarelo', 'Vermelho')), 
+	PRIMARY KEY(IdCartao)
 );
 
 DROP TABLE IF EXISTS Medalha;
 CREATE TABLE Medalha(
-	IDMedalha REFERENCES Ocorrencia, 
-	IDAtleta REFERENCES Atleta, 
-	tipo  VARCHAR(10) CHECK(tipo in ('Bronze', 'Prata', 'Ouro')), 
-	PRIMARY KEY(IDMedalha, IDAtleta)
+	IdMedalha REFERENCES Ocorrencia ON DELETE SET NULL ON UPDATE CASCADE, 
+	Tipo  VARCHAR(10) CHECK(Tipo in ('Bronze', 'Prata', 'Ouro')), 
+	PRIMARY KEY(IdMedalha)
 );
 
 DROP TABLE IF EXISTS Desistencia;
 CREATE TABLE Desistencia(
-	iDesistencia REFERENCES Ocorrencia, 
-	iDAtleta REFERENCES Atleta,
-	PRIMARY KEY(IDesistencia, IDAtleta)
+	IdDesistencia REFERENCES Ocorrencia ON DELETE SET NULL ON UPDATE CASCADE, 
+	PRIMARY KEY(IdDesistencia)
 );
 
 
 DROP TABLE IF EXISTS Competicao;
 CREATE TABLE Competicao(
-	IDCompeticao INTEGER PRIMARY KEY, 
-	data TEXT,
-	genero VARCHAR(1) CHECK(genero in ('M','F'))
+	IdCompeticao INTEGER PRIMARY KEY, 
+	Data TEXT,
+	Genero VARCHAR(1) CONSTRAINT generoNaoExistente CHECK(Genero in ('M','F'))
 );
 
 DROP TABLE IF EXISTS Olimpiada;
 CREATE TABLE Olimpiada(
-	ano INTEGER  CONSTRAINT anoOlimpico CHECK(ano in ('2016')), 
-	cidade VARCHAR(20) CONSTRAINT cidadeOlimpica CHECK(cidade in ('Rio de Janeiro')), 
+	Ano INTEGER  PRIMARY KEY CONSTRAINT anoOlimpico CHECK(Ano in ('2016')), 
+	Cidade VARCHAR(20) CONSTRAINT cidadeOlimpica CHECK(Cidade in ('Rio de Janeiro')), 
 	DataInicial DATE, 
-	DataFinal DATE, 
-	PRIMARY KEY(ano, cidade), 
-	CONSTRAINT dataOlimpica CHECK(DataInicial < DataFinal)
-);
+	DataFinal DATE,  
+	CONSTRAINT dataOlimpica CHECK(JULIANDAY(DataInicial) < JULIANDAY(DataFinal))
+);	
 
 DROP TABLE IF EXISTS Participacao;
 CREATE TABLE Participacao(
-	idAtleta REFERENCES Atleta, 
+	IdAtleta REFERENCES Atleta ON DELETE SET NULL ON UPDATE CASCADE, 
+	IdCompeticao REFERENCES Competicao ON DELETE SET NULL ON UPDATE CASCADE,
 	TempoNatacao TEXT DEFAULT '-',
 	TempoCiclismo TEXT DEFAULT '-',
 	TempoCorrida TEXT DEFAULT '-',
 	TempoTotal TEXT DEFAULT '-',
-	posicao VARCHAR(3) DEFAULT '-'
+	Posicao VARCHAR(3) DEFAULT '-',
+	PRIMARY KEY(IdAtleta)
 );
 
 DROP TABLE IF EXISTS ComissaoTecnica;
 CREATE TABLE ComissaoTecnica(
-	idComissao INTEGER PRIMARY KEY, 
+	IdComissao INTEGER PRIMARY KEY, 
 	NumeroDeTecnicos INTEGER, 
-	comitiva REFERENCES ComitivaOlimpica(pais)
+	IdComitiva REFERENCES ComitivaOlimpica ON DELETE SET NULL ON UPDATE CASCADE
 );
 
--- povoar.sql
+++ povoar.sql
@@ -1,39 +1,47 @@
-INSERT INTO Pessoa VALUES(1, 'Jonathan Brownlee', 180, 'M', 78.4, 'Britânica');
-INSERT INTO Pessoa VALUES(2, 'Alistair Brownlee', 183, 'M', 74.2, 'Britânica');
-INSERT INTO Pessoa VALUES(3, 'Henri Schoeman', 167, 'M', 65.6, 'Sul-africana');
-INSERT INTO Pessoa VALUES(4, 'Gwen Jorgensen', 165, 'F', 54.0, 'Estadunidense');
-INSERT INTO Pessoa VALUES(5, 'Nicola Spirig Hug', 158, 'F', 49.3, 'Suíça');
-INSERT INTO Pessoa VALUES(6, 'Vicky Holland', 156, 'F', 78.4, 'Britânica');
-INSERT INTO Pessoa VALUES(7, 'João José Pereira', 182, 'M', 82.3, 'Portuguesa');
-INSERT INTO Pessoa VALUES(8, 'Marten van Riel', 186, 'M', 84.5, 'Belga');
-INSERT INTO Pessoa VALUES(9, 'Mario Mola', 158, 'M', 63.1, 'Espanhola');
-INSERT INTO Pessoa VALUES(10, 'Aaron Royle', 184, 'M', 87.4, 'Australiana');
-INSERT INTO Pessoa VALUES(11, 'Alessandro Fabian', 184, 'M', 82.4, 'Italiana');
-INSERT INTO Pessoa VALUES(12, 'Tyler Mislawchuk', 187, 'M', 85.7, 'Canadiana');
-INSERT INTO Pessoa VALUES(13, 'Bárbara Riveros', 180, 'F', 78.4, 'Chilena');
-INSERT INTO Pessoa VALUES(14, 'Emma Moffatt', 180, 'F', 78.4, 'Australiana');
-INSERT INTO Pessoa VALUES(15, 'Claudia Rivas', 180, 'F', 78.4, 'Mexicana');
-INSERT INTO Pessoa VALUES(16, 'Agnieszka Jerzyk', 180, 'F', 78.4, 'Polonesa');
-INSERT INTO Pessoa VALUES(17, 'Sarah True', 180, 'F', 78.4, 'Estadunidense');
-INSERT INTO Pessoa VALUES(18, 'Pâmella Oliveira', 180, 'F', 78.4, 'Brasileira');
-INSERT INTO Pessoa VALUES(19, 'João Pedro Silva', 180, 'M', 78.4, 'Portuguesa');
-INSERT INTO Pessoa VALUES(20, 'Alexander Bryukhankov', 180, 'M', 78.4, 'Russa');
+PRAGMA foreign_keys = ON;
 
-INSERT INTO Pessoa VALUES(101, 'Roberto Menescal', 180, 'M', 78.4, 'Brasileira');
-INSERT INTO Pessoa VALUES(102, 'Alberto Barretto', 180, 'M', 78.4, 'Brasileira');
-INSERT INTO Pessoa VALUES(103, 'Tércia Figueiredo', 180, 'M', 78.4, 'Brasileira');
-INSERT INTO Pessoa VALUES(104, 'Kenya Parkinson', 180, 'M', 78.4, 'Neozelandesa');
-INSERT INTO Pessoa VALUES(105, 'Marguerite Moreno', 180, 'M', 78.4, 'Portuguesa');
-INSERT INTO Pessoa VALUES(106, 'Darcy Riggs', 180, 'M', 78.4, 'Estadunidense');
-INSERT INTO Pessoa VALUES(107, 'Anis Ball', 180, 'M', 78.4, 'Botsuana');
-INSERT INTO Pessoa VALUES(108, 'Celia Alvarado', 180, 'M', 78.4, 'Colombiana');
+INSERT INTO Pessoa VALUES(1, 'Jonathan Brownlee', 180, 'M', 78.4, 34, 'Britânica');
+INSERT INTO Pessoa VALUES(2, 'Alistair Brownlee', 183, 'M', 74.2, 35, 'Britânica');
+INSERT INTO Pessoa VALUES(3, 'Henri Schoeman', 167, 'M', 65.6, 23, 'Sul-africana');
+INSERT INTO Pessoa VALUES(4, 'Gwen Jorgensen', 165, 'F', 54.0,  27, 'Estadunidense');
+INSERT INTO Pessoa VALUES(5, 'Nicola Spirig Hug', 158, 'F', 49.3, 19, 'Suíça');
+INSERT INTO Pessoa VALUES(6, 'Vicky Holland', 156, 'F', 78.4, 21, 'Britânica');
+INSERT INTO Pessoa VALUES(7, 'João José Pereira', 182, 'M', 82.3, 27, 'Portuguesa');
+INSERT INTO Pessoa VALUES(8, 'Marten van Riel', 186, 'M', 84.5, 24, 'Belga');
+INSERT INTO Pessoa VALUES(9, 'Mario Mola', 158, 'M', 63.1, 22, 'Espanhola');
+INSERT INTO Pessoa VALUES(10, 'Aaron Royle', 184, 'M', 87.4, 30, 'Australiana');
+INSERT INTO Pessoa VALUES(11, 'Alessandro Fabian', 184, 'M', 82.4, 28, 'Italiana');
+INSERT INTO Pessoa VALUES(12, 'Tyler Mislawchuk', 187, 'M', 85.7, 19, 'Canadiana');
+INSERT INTO Pessoa VALUES(13, 'Bárbara Riveros', 156, 'F', 65.4, 29, 'Chilena');
+INSERT INTO Pessoa VALUES(14, 'Emma Moffatt', 162, 'F', 54.7, 21, 'Australiana');
+INSERT INTO Pessoa VALUES(15, 'Claudia Rivas', 153, 'F', 46.6, 31, 'Mexicana');
+INSERT INTO Pessoa VALUES(16, 'Agnieszka Jerzyk', 156, 'F', 56.4, 27, 'Polonesa');
+INSERT INTO Pessoa VALUES(17, 'Sarah True', 161, 'F', 57.1, 34, 'Estadunidense');
+INSERT INTO Pessoa VALUES(18, 'Pâmella Oliveira', 162, 'F', 57.3, 29,  'Brasileira');
+INSERT INTO Pessoa VALUES(19, 'João Pedro Silva', 178, 'M', 74.1, 31, 'Portuguesa');
+INSERT INTO Pessoa VALUES(20, 'Alexander Bryukhankov', 187, 'M', 85.9, 35, 'Russa');
 
-INSERT INTO Pessoa VALUES(201, 'Pedro Castro', NULL, 'M', NULL, 'Portuguesa');
-INSERT INTO Pessoa VALUES(202, 'Beatriz Rocha', NULL, 'F', NULL, 'Brasileira');
-INSERT INTO Pessoa VALUES(203, 'Bianca Tamburini', NULL, 'F', NULL, 'Italiana');
-INSERT INTO Pessoa VALUES(204, 'Louis Murphy', NULL, 'M', NULL, 'Britânica');
-INSERT INTO Pessoa VALUES(205, 'Marvin Irwin', NULL, 'M', NULL, 'Britânica');
+INSERT INTO Pessoa VALUES(101, 'Roberto Menescal', 173, 'M', 78.2, 41, 'Brasileira');
+INSERT INTO Pessoa VALUES(102, 'Alberto Barretto', 173, 'M', 70.1, 45, 'Brasileira');
+INSERT INTO Pessoa VALUES(103, 'Tércia Figueiredo', 172, 'M', 64.5, 37, 'Brasileira');
+INSERT INTO Pessoa VALUES(104, 'Kenya Parkinson', 181, 'M', 72.8, 48, 'Neozelandesa');
+INSERT INTO Pessoa VALUES(105, 'Marguerite Moreno', 167, 'M', 55.2, 43, 'Portuguesa');
+INSERT INTO Pessoa VALUES(106, 'Darcy Riggs', 168, 'M', 62.0, 54, 'Estadunidense');
+INSERT INTO Pessoa VALUES(107, 'Anis Ball', 165, 'M', 64.9, 42, 'Botsuana');
+INSERT INTO Pessoa VALUES(108, 'Celia Alvarado', 165, 'M', 52.1,39, 'Colombiana');
 
+
+
+INSERT INTO Pessoa(IdPessoa, Nome, Genero, Nacionalidade) VALUES(201, 'Pedro Castro', 'M', 'Portuguesa');
+INSERT INTO Pessoa(IdPessoa, Nome, Genero, Nacionalidade) VALUES(202, 'Beatriz Rocha', 'F', 'Brasileira');
+INSERT INTO Pessoa(IdPessoa, Nome, Genero, Nacionalidade) VALUES(203, 'Bianca Tamburini', 'F', 'Italiana');
+INSERT INTO Pessoa(IdPessoa, Nome, Genero, Nacionalidade) VALUES(204, 'Louis Murphy', 'M', 'Britânica');
+INSERT INTO Pessoa(IdPessoa, Nome, Genero, Nacionalidade) VALUES(205, 'Marvin Irwin', 'M', 'Britânica');
+
+
+INSERT INTO Competicao VALUES(501, '2016-08-18',  'M');
+INSERT INTO Competicao VALUES(502, '2016-08-20',  'F');
+  
 
 INSERT INTO ComitivaOlimpica VALUES(1, 'Portugal', 'Triatlo');
 INSERT INTO ComitivaOlimpica VALUES(2, 'Grã-Bretanha', 'Triatlo');
@@ -51,26 +59,26 @@ INSERT INTO ComitivaOlimpica VALUES(13, 'África do Sul', 'Triatlo');
 INSERT INTO ComitivaOlimpica VALUES(14, 'Suíça', 'Triatlo');
 INSERT INTO ComitivaOlimpica VALUES(15, 'Rússia', 'Triatlo');
 
-INSERT INTO Atleta VALUES(10001,2, 1, 501);
-INSERT INTO Atleta VALUES(10002,2, 2, 501);
-INSERT INTO Atleta VALUES(10003,13, 3, 501);
-INSERT INTO Atleta VALUES(10004,5, 4, 502);
-INSERT INTO Atleta VALUES(10005,14, 5, 502);
-INSERT INTO Atleta VALUES(10006,2, 6, 502);
-INSERT INTO Atleta VALUES(10007,1, 7, 501);
-INSERT INTO Atleta VALUES(10008,10, 8, 501);
-INSERT INTO Atleta VALUES(10009,12, 9, 501);
-INSERT INTO Atleta VALUES(10010,7, 10, 501);
-INSERT INTO Atleta VALUES(10011,8, 11, 501);
-INSERT INTO Atleta VALUES(10012,3, 12, 501);
-INSERT INTO Atleta VALUES(10013,11, 13, 502);
-INSERT INTO Atleta VALUES(10014,7, 14, 502);
-INSERT INTO Atleta VALUES(10015,6, 15, 502);
-INSERT INTO Atleta VALUES(10016,9, 16, 502);
-INSERT INTO Atleta VALUES(10017,5, 17, 502);
-INSERT INTO Atleta VALUES(10018,4, 18, 502);
-INSERT INTO Atleta VALUES(10019,1, 19, 501);
-INSERT INTO Atleta VALUES(10020,14, 20, 501);
+INSERT INTO Atleta VALUES(2, 1, 501);
+INSERT INTO Atleta VALUES(2, 2, 501);
+INSERT INTO Atleta VALUES(13, 3, 501);
+INSERT INTO Atleta VALUES(5, 4, 502);
+INSERT INTO Atleta VALUES(14, 5, 502);
+INSERT INTO Atleta VALUES(2, 6, 502);
+INSERT INTO Atleta VALUES(1, 7, 501);
+INSERT INTO Atleta VALUES(10, 8, 501);
+INSERT INTO Atleta VALUES(12, 9, 501);
+INSERT INTO Atleta VALUES(7, 10, 501);
+INSERT INTO Atleta VALUES(8, 11, 501);
+INSERT INTO Atleta VALUES(3, 12, 501);
+INSERT INTO Atleta VALUES(11, 13, 502);
+INSERT INTO Atleta VALUES(7, 14, 502);
+INSERT INTO Atleta VALUES(6, 15, 502);
+INSERT INTO Atleta VALUES(9, 16, 502);
+INSERT INTO Atleta VALUES(5, 17, 502);
+INSERT INTO Atleta VALUES(4, 18, 502);
+INSERT INTO Atleta VALUES(1, 19, 501);
+INSERT INTO Atleta VALUES(14, 20, 501);
 
 
 INSERT INTO Tecnico VALUES(1, 201);
@@ -79,18 +87,16 @@ INSERT INTO Tecnico VALUES(8, 203);
 INSERT INTO Tecnico VALUES(2, 204);
 INSERT INTO Tecnico VALUES(2, 205);
 
-INSERT INTO Arbitro VALUES(100, 'Geral');
-INSERT INTO Arbitro VALUES(101, 'Natação');
-INSERT INTO Arbitro VALUES(102, 'Transição');
-INSERT INTO Arbitro VALUES(103, 'Ciclismo');
-INSERT INTO Arbitro VALUES(104, 'Corrida');
-INSERT INTO Arbitro VALUES(105, 'Cronometragem');
-INSERT INTO Arbitro VALUES(106, 'Geral');
-INSERT INTO Arbitro VALUES(107, 'Natação');
-  
 
-INSERT INTO Competicao VALUES(501, '2016-08-18',  'M');
-INSERT INTO Competicao VALUES(502, '2016-08-20',  'F');
+
+INSERT INTO Arbitro VALUES(101, 'Geral');
+INSERT INTO Arbitro VALUES(102, 'Natação');
+INSERT INTO Arbitro VALUES(103, 'Transição');
+INSERT INTO Arbitro VALUES(104, 'Ciclismo');
+INSERT INTO Arbitro VALUES(105, 'Corrida');
+INSERT INTO Arbitro VALUES(106, 'Cronometragem');
+INSERT INTO Arbitro VALUES(107, 'Geral');
+INSERT INTO Arbitro VALUES(108, 'Natação');
   
 INSERT INTO Prova VALUES(601, 501, 'Natação');
 INSERT INTO Prova VALUES(602, 501, 'Ciclismo');
@@ -99,29 +105,48 @@ INSERT INTO Prova VALUES(604, 502, 'Natação');
 INSERT INTO Prova VALUES(605, 502, 'Ciclismo');
 INSERT INTO Prova VALUES(606, 502, 'Corrida');
 
-INSERT INTO Ocorrencia(idOcorrencia, tempo) VALUES(1001, '01:45:01');
-INSERT INTO Ocorrencia VALUES(1002, '00:17:31', 'Desistência por Cansaço');
-INSERT INTO Ocorrencia(idOcorrencia, tempo) VALUES(1003, '01:45:07');
-INSERT INTO Ocorrencia(idOcorrencia, tempo) VALUES(1004, '00:43:56');
+INSERT INTO Ocorrencia(IdOcorrencia, IdAtleta,IdCompeticao, Tempo) VALUES(1001, 1, 501, '01:45:01');
+INSERT INTO Ocorrencia VALUES(1002, 7, 501, '00:17:31', 'Desistência por fadiga');
+INSERT INTO Ocorrencia(IdOcorrencia,  IdAtleta,IdCompeticao, Tempo) VALUES(1003, 2, 501,  '01:45:07');
+INSERT INTO Ocorrencia(IdOcorrencia, IdAtleta, IdCompeticao, Tempo) VALUES(1004,  5, 501, '00:43:56');
+INSERT INTO Ocorrencia(IdOcorrencia, IdAtleta, IdCompeticao, Tempo) VALUES(1006,  13, 502, '01:28:23');;
+INSERT INTO Ocorrencia(IdOcorrencia, IdAtleta, IdCompeticao, Tempo) VALUES(1007,  4, 502, '01:56:16');
+INSERT INTO Ocorrencia(IdOcorrencia, IdAtleta, IdCompeticao, Tempo) VALUES(1009,  3, 501, '01:45:32');
+INSERT INTO Ocorrencia(IdOcorrencia, IdAtleta, IdCompeticao, Tempo) VALUES(1010,  5, 502, '01:56:56');
+INSERT INTO Ocorrencia VALUES(1008,  18, 502, '01:43:56', 'Desistência provocada por acidente durante a Competição');
+INSERT INTO Ocorrencia(idOcorrencia, IdAtleta, IdCompeticao,Tempo) VALUES(1011,  6, 502, '01:57:01');
 
-INSERT INTO Cartao VALUES(1004, 10005, 'Amarelo');
+INSERT INTO Cartao VALUES(1004, 'Amarelo');
+INSERT INTO Cartao VALUES(1006, 'Vermelho');
+
+INSERT INTO Medalha VALUES(1001, 'Ouro');
+INSERT INTO Medalha VALUES(1003, 'Prata');
+INSERT INTO Medalha VALUES(1009, 'Bronze');
+
+INSERT INTO Medalha VALUES(1007, 'Ouro');
+INSERT INTO Medalha VALUES(1010, 'Prata');
+INSERT INTO Medalha VALUES(1011, 'Bronze');
 
 
-INSERT INTO Medalha VALUES(1001, 10001, 'Ouro');
-INSERT INTO Medalha VALUES(1003, 10002, 'Prata');
+INSERT INTO Desistencia VALUES(1002);
+INSERT INTO Desistencia VALUES(1008);
 
-INSERT INTO Desistencia VALUES(1002, 10007);
+INSERT INTO Olimpiada VALUES(2016, 'Rio de Janeiro', '2016-08-05', '2016-08-21');
 
-INSERT INTO Olimpiada VALUES(2016, 'Rio de Janeiro', JULIANDAY('2016-08-05'), JULIANDAY('2016-08-21'));
+INSERT INTO Participacao VALUES(1, 501,'00:17:24', '00:55:04', '00:31:09', '1:45:01', 1);
+INSERT INTO Participacao VALUES(2, 501, '00:17:24', '00:55:04', '00:31:16', '1:45:07', 2);
+INSERT INTO Participacao(IdAtleta, IdCompeticao, TempoNatacao) VALUES(10, 501, '00:17:24');
+INSERT INTO Participacao VALUES(3, 501, '00:17:25', '00:55:01', '00:31:50', '1:45:43', 3);
+INSERT INTO Participacao(IdAtleta, IdCompeticao, TempoNatacao, TempoCiclismo)  VALUES(18, 502, '00:21:09', '01:09:03');
+INSERT INTO Participacao VALUES(4, 502, '00:19:12', '01:01:21', '00:34:09', '1:56:16', 1);
+INSERT INTO Participacao VALUES(5, 502, '00:19:12', '01:01:22', '00:34:50', '1:56:56', 2);
+INSERT INTO Participacao VALUES(6, 502, '00:19:09', '01:01:26', '00:34:54', '1:57:01', 3);
+INSERT INTO Participacao VALUES(7, 501, '00:18:03', '00:55:52', '00:30:38', '1:45:52', 5);
 
-INSERT INTO Participacao VALUES(10001, '00:17:24', '00:55:04', '00:31:09', '1:45:01', 1);
-INSERT INTO Participacao VALUES(10002, '00:17:24', '00:55:04', '00:31:16', '1:45:07', 2);
-INSERT INTO Participacao VALUES(10007, '00:18:03', '00:55:52', '00:30:38', '1:45:52', 5);
-
-INSERT INTO ComissaoTecnica VALUES(7001, 18, 'Estados Unidos');
-INSERT INTO ComissaoTecnica VALUES(7002, 9, 'África do Sul');
-INSERT INTO ComissaoTecnica VALUES(7003, 12, 'Portugal');
-INSERT INTO ComissaoTecnica VALUES(7004, 15, 'Brasil');
+INSERT INTO ComissaoTecnica VALUES(7001, 18, 5);
+INSERT INTO ComissaoTecnica VALUES(7002, 9, 13);
+INSERT INTO ComissaoTecnica VALUES(7003, 12, 1);
+INSERT INTO ComissaoTecnica VALUES(7004, 15, 4);
   
   
   
